fed35855839c94c5f611dad51bcc42c42769a8de
Berenice D'Alleva <bdalleva@frro.utn.edu.ar>
2024-05-09 17:00:43 -0300

    primer cambio

---
+++ src/app.ts
@@ -0,0 +1,107 @@
+import express, { Express, NextFunction, Response,Request } from "express";
+import { TipoCliente } from "./tiposcliente.js";
+import {it} from 'node:test'
+
+
+export const app = express()
+app.use(express.json())
+
+//user -- request --express -- express.json() -- app.post (req.body) -- response -- user
+//get /api/characters/ -- obtener lalista de characters
+//get /api/characters/:id -- obtener el character con id = :id
+//post /api/characters -- crear un nuevo character
+//delete /api/characters/:id  -- eliminar character con id = :id
+//put & patch /api/characters/:id -- modificar character con id = :id
+
+export const tiposcliente= [
+  new TipoCliente(
+  'Minorista',
+  'Cliente sin compra minima',
+  0,
+  'user-minorista-2024-1234-dsw',
+),
+]
+
+function sanitizeTipoClienteInput(req: Request, res: Response, next: NextFunction){
+  req.body.sanitizedInput = {
+    tipoclienteClass: req.body.tipoclienteClass,
+    desctipo: req.body.desctipo,
+    porcdescuento: req.body.porcdescuento
+  }
+  //more checks here
+
+  next()
+}
+
+//obtener una lista de todos los tipos de cliente
+app.get('/api/tiposcliente',(req,res)=>{
+     res.json({data:tiposcliente})
+})
+
+//obtener un tipo cliente por id
+app.get('/api/tiposcliente/:id',(req,res)=>{
+  const tipocliente = tiposcliente.find((tipocliente)=>tipocliente.id === req.params.id)
+  if(!tipocliente){
+    return res.status(404).send({message:'Cliente no encontrado'})
+  }
+  res.json(tiposcliente)
+})
+
+//crear un tipo cliente
+app.post('/api/tiposcliente', sanitizeTipoClienteInput,(req,res)=>{
+  const input= req.body.sanitizedInput
+  
+  const tipocliente = new TipoCliente(
+    input.tipoclienteClass,
+    input.desctipo,
+    input.porcdescuento
+  )
+
+  tiposcliente.push(tipocliente)
+  return res.status(201).send({message:'Cliente creado exitosamente',data:tipocliente})
+})
+
+//modificar un tipo cliente (todas las propiedades)
+app.put('/api/tiposcliente/:id', (req, res) => {
+  const tipoclienteIdx = tiposcliente.findIndex((tipocliente) => tipocliente.id === req.params.id);
+  
+  if (tipoclienteIdx === -1) {
+    return res.status(404).send({ message: 'Cliente no encontrado' });
+  }
+  
+  Object.assign (tiposcliente[tipoclienteIdx],req.body)
+  return res.status(200).send({message:'Cliente modificado exitosamente', data: tiposcliente[tipoclienteIdx]})
+})
+
+//modificar un tipo cliente(solo algunas propiedades)
+//sanitiseTipoClienteInput no es una funcion de middleware,por eso solo uso req.body
+app.patch('/api/tiposcliente/:id',(req, res) => {
+  const tipoclienteIdx = tiposcliente.findIndex((tipocliente) => tipocliente.id === req.params.id)
+  
+  if (tipoclienteIdx === -1) {
+    return res.status(404).send({ message: 'Cliente no encontrado' });
+  }
+  
+  Object.assign (tiposcliente[tipoclienteIdx],req.body)
+  return res.status(200).send({message:'Cliente modificado exitosamente', data: tiposcliente[tipoclienteIdx]})
+})
+
+//borrar un tipo cliente
+app.delete('/api/tiposcliente/:id', (req, res) => {
+ const tipoclienteIdx = tiposcliente.findIndex((tipocliente) => tipocliente.id === req.params.id)
+ if (tipoclienteIdx === -1) {
+    return res.status(404).send({ message: 'Cliente no encontrado' });
+ } 
+ tiposcliente.splice(tipoclienteIdx,1)
+ return res.status(200).send({message:'Cliente borrado exitosamente'})
+})
+
+//para cuando el url esta mal escrito
+app.use((req, res) => {
+  return res.status(404).send({message: 'Ruta no encontrada'})
+})
+
+//defino puerto en el que se va a correr el servidor
+app.listen(4000,()=>{
+  console.log('Server is running on http://localhost:4000/')
+})
---
+++ src/tiposcliente.ts
@@ -0,0 +1,11 @@
+import  crypto from 'node:crypto';
+import {app, tiposcliente} from "./app";
+
+export class TipoCliente {
+    constructor(
+    public tiposclienteClass: string, 
+    public desctipo: string,
+    public porcdescuento: GLfloat,
+    public id= crypto.randomUUID()
+  ) {}
+}
